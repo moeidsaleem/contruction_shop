cbfffc23381a6d172e780a26158d9f4930c67872
MinhQuan <buikhacminhquan@hotmail.com>
2018-05-06 13:00:15 +0700

    update DB

--- DB/construction_shop.sql
+++ DB/construction_shop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-04-25 18:43:22
+Date: 2018-05-06 12:56:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `clients`;
 CREATE TABLE `clients` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` int(11) DEFAULT NULL,
@@ -31,18 +31,19 @@ CREATE TABLE `clients` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of clients
 -- ----------------------------
+INSERT INTO `clients` VALUES ('1', '123', 'ass', '123', 'sadsadsd', 'ssss', '12', '2018-04-28 15:25:00', '2018-04-28 15:25:26', '2018-04-28 15:25:26');
 
 -- ----------------------------
 -- Table structure for ins
 -- ----------------------------
 DROP TABLE IF EXISTS `ins`;
 CREATE TABLE `ins` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
   `supplier_id` int(11) DEFAULT NULL,
   `amount` int(10) DEFAULT NULL,
@@ -83,7 +84,7 @@ INSERT INTO `log_users` VALUES ('4', '1', '$2y$10$eql/gxt8flfcWbGbCKyDBeZ4SAzsgr
 -- ----------------------------
 DROP TABLE IF EXISTS `outs`;
 CREATE TABLE `outs` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `client_id` int(11) DEFAULT NULL,
   `total_cost` int(20) DEFAULT NULL,
   `paid` int(20) DEFAULT NULL,
@@ -91,11 +92,12 @@ CREATE TABLE `outs` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of outs
 -- ----------------------------
+INSERT INTO `outs` VALUES ('1', '1', '22222', '20', '12', '2018-04-28 15:25:58', '2018-04-28 15:25:58');
 
 -- ----------------------------
 -- Table structure for products
@@ -115,11 +117,13 @@ CREATE TABLE `products` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
+INSERT INTO `products` VALUES ('1', 'a', 'a', '1', '0', '123', 'asds ', '111', '1', 'sads', '2018-04-27 21:46:42', '2018-04-27 21:46:42');
+INSERT INTO `products` VALUES ('4', 'aa', 'a', '1', '0', '44', 'aa', '44', 'aa', 'aa', '2018-05-06 09:17:51', '2018-05-06 09:17:51');
 
 -- ----------------------------
 -- Table structure for product_types
@@ -146,7 +150,7 @@ INSERT INTO `product_types` VALUES ('2', 'tes122222', 'testt', 'tetet', '2018-04
 -- ----------------------------
 DROP TABLE IF EXISTS `sub_product_outs`;
 CREATE TABLE `sub_product_outs` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
   `out_id` int(11) DEFAULT NULL,
   `cost_per_unit` int(20) DEFAULT NULL,
@@ -164,7 +168,7 @@ CREATE TABLE `sub_product_outs` (
 -- ----------------------------
 DROP TABLE IF EXISTS `suppliers`;
 CREATE TABLE `suppliers` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` int(11) DEFAULT NULL,
@@ -172,11 +176,12 @@ CREATE TABLE `suppliers` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of suppliers
 -- ----------------------------
+INSERT INTO `suppliers` VALUES ('1', 'hello', '123', '123', 'asdasd', '2018-04-27 17:02:20', '2018-04-27 17:02:20');
 
 -- ----------------------------
 -- Table structure for users
